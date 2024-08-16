c14ea2c24edbfb3b6e5f2878fdc0485820a5c92b
Enoch Whitaker <enochwhitaker3@gmail.com>
2024-08-16 13:51:13 -0600

    WIP(18): Added hover to posters again

--- frontend/src/components/PosterList.tsx
+++ frontend/src/components/PosterList.tsx
@@ -8,6 +8,7 @@ interface PosterProps {
 
 const Poster: React.FC<PosterProps> = ({ play }) => {
   const [isOverlayVisible, setOverlayVisible] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleClick = () => {
     setOverlayVisible((prev) => !prev);
@@ -39,12 +40,26 @@ const Poster: React.FC<PosterProps> = ({ play }) => {
   );
   const endDate = new Date(play.end_date).toLocaleDateString("en-US", options);
 
+  const handleMouseEnter = () => {
+    if (window.innerWidth > 640) { // 640px corresponds to the 'sm' breakpoint in Tailwind
+      setIsHovered(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (window.innerWidth > 640) {
+      setIsHovered(false);
+    }
+  };
+
   return (
     <div className="w-full">
       <div className="m-8 w-full px-4 mx-auto">
         <div
           className={`relative rounded-lg bg-creame shadow-lg w-18 cursor-pointer group overflow-hidden poster-container`}
           onClick={handleClick}
+          onMouseEnter={() => handleMouseEnter()}
+          onMouseLeave={() => handleMouseLeave()}
         >
           <img
             src={play.poster}
@@ -53,7 +68,7 @@ const Poster: React.FC<PosterProps> = ({ play }) => {
           />
           <div
             className={`absolute bottom-0 left-0 right-0 h-3/4 bg-blackolive transform ${
-              isOverlayVisible ? "translate-y-0" : "translate-y-full"
+              isOverlayVisible || isHovered ? "translate-y-0" : "translate-y-full"
             } transition-transform duration-700 ease-in-out flex flex-col items-center justify-center`}
           >
             <ul className="text-creame px-4 lg:text-lg md:text-sm sm:text-xs">
